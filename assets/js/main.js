1ac79144f7eaae4ecc0d245d6f2d3eac9a737da6
KouroshSalmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-03-03 13:58:50 +0330

    add new backgrounds and edit done list css #6

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,18 @@ const closeIcon = document.querySelector('.modal svg');
 /////// To do list \\\\\\\
 const toDoDiv = document.querySelector('.to-do');
 const tarshBin = `<lord-icon
-class="trach-bin"
+class="trash-bin"
 src="https://cdn.lordicon.com/drxwpfop.json"
 trigger="hover"
 colors="primary:#fff,secondary:#22faa2"
+style="width:30px;height:30px">
+</lord-icon>`;
+const trashBinTicked = `<lord-icon
+class="trash-bin-ticked"
+src="https://cdn.lordicon.com/drxwpfop.json"
+trigger="morph"
+state="morph-trash-in"
+colors="primary:#000,secondary:#000"
 style="width:30px;height:30px">
 </lord-icon>`;
 const tickIcon = `<lord-icon
@@ -41,7 +49,7 @@ let done = [];
 
 document.addEventListener('DOMContentLoaded', function () {
     function randomBackground() {
-        return Math.floor(Math.random() * 15);
+        return Math.floor(Math.random() * 85);
     }
 
     /////// Add random Background image \\\\\\\
@@ -268,14 +276,21 @@ function tickToDo() {
         done.push(newTodo);
 
         const titleDoneDiv = `<span class="title-done">Done List</span>`;
-        const lastToDo = `<li class="to-do-item">
-            <div class="id-title"><span class="id">${newTodo.id}</span>- <span>${newTodo.title}</span></div>
-            <div class="description">${newTodo.description}</div>
-            <div class="manage-item">${tarshBin}</div></li>`;
+        const lastToDo = `<div class="container-to-do">
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0,0,256,256"
+        style="fill:#000000;">
+        <defs><linearGradient x1="21.241" y1="39.241" x2="3.541" y2="21.541" gradientUnits="userSpaceOnUse" id="color-1_VFaz7MkjAiu0_gr1"><stop offset="0.108" stop-color="#ff8282"></stop><stop offset="0.433" stop-color="#000000"></stop></linearGradient><linearGradient x1="44.41029" y1="9.58989" x2="14.59" y2="39.41123" gradientUnits="userSpaceOnUse" id="color-2_VFaz7MkjAiu0_gr2"><stop offset="0" stop-color="#000000"></stop><stop offset="1" stop-color="#00ff95"></stop></linearGradient></defs><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.33333,5.33333)"><path d="M16.599,41.42l-15.019,-15.019c-0.774,-0.774 -0.774,-2.028 0,-2.802l4.019,-4.019c0.774,-0.774 2.028,-0.774 2.802,0l15.019,15.019c0.774,0.774 0.774,2.028 0,2.802l-4.019,4.019c-0.774,0.773 -2.028,0.773 -2.802,0z" fill="url(#color-1_VFaz7MkjAiu0_gr1)"></path><path d="M12.58,34.599l27.019,-27.019c0.774,-0.774 2.028,-0.774 2.802,0l4.019,4.019c0.774,0.774 0.774,2.028 0,2.802l-27.019,27.019c-0.774,0.774 -2.028,0.774 -2.802,0l-4.019,-4.019c-0.773,-0.774 -0.773,-2.028 0,-2.802z" fill="url(#color-2_VFaz7MkjAiu0_gr2)"></path></g></g>
+        </svg>
+        <li class="to-do-item">
+        <div class="id-title"><span class="id">${newTodo.id}</span>- <span>${newTodo.title}</span></div>
+        <div class="description">${newTodo.description}</div>
+        <div class="manage-item">${trashBinTicked}</div></li>
+        </div>`;
 
         if (doneDiv.innerHTML == '') {
+            debugger
             doneDiv.insertAdjacentHTML('afterbegin', titleDoneDiv);
-            doneDiv.lastElementChild.insertAdjacentHTML('afterend', lastToDo);
+            doneDiv.insertAdjacentHTML('beforeend', lastToDo);
             doneDiv.classList.add('active');
         } else {
             doneDiv.lastElementChild.insertAdjacentHTML('afterend', lastToDo);
@@ -294,7 +309,6 @@ function tickToDo() {
     const tickIcons = document.querySelectorAll('.manage-item .tick');
 
     tickIcons.forEach(tickIcon => {
-        debugger
         tickIcons.forEach(tickIcon => {
             tickIcon.removeEventListener('click', tickToDoItemHandler);
         });
